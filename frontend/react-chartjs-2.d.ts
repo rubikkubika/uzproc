5451634efec74934039f8d00b628fafe66c742fb
Artem <artem@example.com>
2025-11-20 17:33:57 +0500

    Initial commit: uzproc project

---
+++ frontend/react-chartjs-2.d.ts
@@ -0,0 +1,23 @@
+declare module 'react-chartjs-2' {
+  import { Component } from 'react';
+  import { ChartData, ChartOptions } from 'chart.js';
+
+  export interface ChartComponentProps {
+    data: ChartData;
+    options?: ChartOptions;
+    redraw?: boolean;
+    getDatasetAtEvent?: (dataset: any) => void;
+    getElementAtEvent?: (element: any) => void;
+    getElementsAtEvent?: (elements: any) => void;
+  }
+
+  export class Line extends Component<ChartComponentProps> {}
+  export class Bar extends Component<ChartComponentProps> {}
+  export class Pie extends Component<ChartComponentProps> {}
+  export class Doughnut extends Component<ChartComponentProps> {}
+  export class Radar extends Component<ChartComponentProps> {}
+  export class PolarArea extends Component<ChartComponentProps> {}
+  export class Bubble extends Component<ChartComponentProps> {}
+  export class Scatter extends Component<ChartComponentProps> {}
+}
+
